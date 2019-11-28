cb939dbef503fa6530f96149f324534e5495315b
frederik7741 <frederiksbox@gmail.com>
2019-11-28 09:59:45 +0100

    done

--- billar-spin/sketch.js
+++ billar-spin/sketch.js
@@ -6,11 +6,17 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight); 
+  imageMode(CENTER)
+  createCanvas(300, 300);
+
+
 }
 
 function draw() {
-  background(220);
-image(img, 0, 0);
+  background(300);//260 vis du vil kunne se canvas
+    translate(150, 150);
+    frameRate(100);
+    rotate(radians(frameCount));
+  image(img, 0, 0, 200, 175);
 
 }
